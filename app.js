1a2e50b30c288d876151abf9d6fb9a6cbc9f53ac
jimmynewsom <jimmy.newsom@icloud.com>
2018-02-26 12:05:11 -0800

    test

--- app.js
+++ app.js
@@ -10,26 +10,24 @@ const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
 const app = express();
-
 app.use(helmet());
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(expressSanitizer());
-
 app.use(express.static('public'));
 
-MongoClient.connect(process.env.MONGODB_URI, function(err, db) {
+var message;
 
-    var message;
+MongoClient.connect(process.env.MONGODB_URI, function(err, db) {
+    assert.equal(null, err);
+    console.log("connected successfully to server");
 
     app.get('/', (req, res) => 
     {   
         db.collection('storage').findOne({}, {message:1}, function(err, doc){
+            console.log(doc);
             message = doc.message;
             res.render('index', {message: message});
         });
@@ -38,6 +36,7 @@ MongoClient.connect(process.env.MONGODB_URI, function(err, db) {
     app.post('/message_received', (req, res) => {  //send a message to a stranger :)
         message = req.sanitize(req.body.message);
         db.collection('storage').findOneAndUpdate({}, {message: message}, function(err, result){
+            console.log(result);
             res.sendFile('message_received.html', {root: 'views'});
         });
     });
