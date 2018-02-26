7bd0318a259ca085585ea7fa97ecd0a7cdf44df9
jimmynewsom <jimmy.newsom@icloud.com>
2018-02-26 12:45:31 -0800

    test3

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var bodyParser = require('body-parser');
 var expressSanitizer = require('express-sanitizer');
 
 //there's better ways to do this than MongoDB. lots of dead weight.
-// but I can do this on Heroku for free, so fuck it
+// but I can do this for free on Heroku, so fuck it
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
@@ -18,14 +18,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(expressSanitizer());
 app.use(express.static('public'));
 
-var message;
-
 MongoClient.connect(process.env.MONGODB_URI, (err, database) =>
 {
     assert.equal(null, err);
     console.log("connected successfully to server");
-
-    console.log(database);
 
     const db = database.db('heroku_8728mxs3');
 
@@ -33,16 +29,13 @@ MongoClient.connect(process.env.MONGODB_URI, (err, database) =>
     {   
         db.collection('storage').findOne({}, {message : 1, _id : 0}, (err, doc) => 
         {
-            console.log(doc);
-            message = doc.message;
-            res.render('index', {message: message});
+            res.render('index', {message: doc.message});
         });
     });
 
-    app.post('/message_received', (req, res, db) => {  //send a message to a stranger :)
-        message = req.sanitize(req.body.message);
+    app.post('/message_received', (req, res) => {  //send a message to a stranger :)
+        let message = req.sanitize(req.body.message);
         db.collection('storage').findOneAndUpdate({}, {message: message}, function(err, result){
-            console.log(result);
             res.sendFile('message_received.html', {root: 'views'});
         });
     });
